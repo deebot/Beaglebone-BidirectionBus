58e3fd30dfb098ed1f3cb1902bae2a4930868876
Deepankar Maithani <deepankar19910@gmail.com>
2020-08-18 01:14:46 +0000

    Modified unit file,added reset script and readme

---
+++ bidirec_299/pru_pinconfig/PRUdefault.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+config-pin P2.34 default
+config-pin P2.32 default
+config-pin P1.33 default
+config-pin P1.31 default
+config-pin P2.30 default
+config-pin P2.28 default
+config-pin P2.18 default
+
+
